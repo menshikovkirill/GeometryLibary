491c9503cd19d2250db863af10c046b8e116cd0d
menshikovkirill <43323676+menshikovkirill@users.noreply.github.com>
2020-04-26 00:37:50 +0300

    Update Program.cs

--- Geometry1/Program.cs
+++ Geometry1/Program.cs
@@ -10,8 +10,7 @@ namespace Geometry1
     {
         static void Main(string[] args)
         {
-            double r = 2;
-            Console.WriteLine(Math.Sqrt(2) * Math.Sqrt(2) - 2 < 10E-16);
+            
         }
 
         //возможно под "Вычисление площади фигуры без знания типа фигуры" имелось ввиду, что до вычисления площади мы не знаем, какая именно фигура придет на вход
